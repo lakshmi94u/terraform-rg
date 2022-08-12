b0969e1531f456638c4f89e9c470bc09a0f07711
lakshmi94u <lakshmi94u@gmail.com>
2022-08-12 19:47:29 +0530

    first commit

---
+++ compute/resource_group/outputs.tf
@@ -0,0 +1,4 @@
+output "resource_group_name" {
+  description = "resouce group id"
+  value       = azurerm_resource_group.rg.name
+}
---
+++ compute/resource_group/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group" {
+    type = string
+    description = "name of the resorce group" 
+}
+
+variable "location" {
+    type = string
+  description = "name of the location"
+}
